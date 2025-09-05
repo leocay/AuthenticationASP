aa4fa5dc0328b0992f1dc881d845881ba80aedad
Duc Linh <linhtd.caet@gmail.com>
2025-09-05 16:42:10 +0700

    update 0509

---
+++ AuthenService/Controllers/ApiController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using ErrorOr;
+
+namespace AuthenService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiController : ControllerBase
+    {
+        protected IActionResult Problem(List<ErrorOr.Error> errors)
+        {
+            if (errors.Count is 0)
+            {
+                return Problem();
+            }
+
+            if (errors.All(errors => errors.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+
+            HttpContext.Items["errors"] = errors;
+
+            return Problem(errors.FirstOrDefault());
+        }
+
+        private IActionResult Problem(ErrorOr.Error error)
+        {
+            var statusCode = error.Type switch
+            {
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError,
+            };
+
+            return Problem(statusCode: statusCode, title: error.Description);
+        }
+
+        private IActionResult ValidationProblem(List<ErrorOr.Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem(modelStateDictionary);
+        }
+    }
+}
